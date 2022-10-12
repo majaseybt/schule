19c088ffb01a185bbd72218bad1ec586bf191613
majaseybt <115649157+majaseybt@users.noreply.github.com>
2022-10-12 19:32:04 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(randint(1, 6))
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.showString("maja")
+})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.turnRgbLedOff()
     x = randint(1, 6)
@@ -10,8 +13,12 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         music.playTone(440, music.beat(BeatFraction.Whole))
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("hi!")
+})
 let x = 0
-basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.Heart)
+basic.setLedColor(0x00ffff)
 basic.forever(function () {
     if (input.lightLevel() < 100) {
         basic.setLedColor(0x00ffff)
