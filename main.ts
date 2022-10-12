0fb8926a0d6670f3f828ca069b5763e561e22987
majaseybt <115649157+majaseybt@users.noreply.github.com>
2022-10-12 18:31:41 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(randint(1, 6))
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.turnRgbLedOff()
     x = randint(1, 6)
     basic.showNumber(x)
     if (x == 6) {
+        basic.setLedColor(0x00ffff)
         music.playTone(440, music.beat(BeatFraction.Whole))
     }
 })
