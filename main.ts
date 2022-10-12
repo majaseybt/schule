bde45c92c6c0a931c7da70fb3e4509816a1330a8
majaseybt <115649157+majaseybt@users.noreply.github.com>
2022-10-12 18:30:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    x = randint(1, 6)
+    basic.showNumber(x)
+    if (x == 6) {
+        music.playTone(440, music.beat(BeatFraction.Whole))
+    }
+})
+let x = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    if (input.lightLevel() < 100) {
+        basic.setLedColor(0x00ffff)
+    } else {
+        basic.turnRgbLedOff()
+    }
+    basic.pause(100)
 })
